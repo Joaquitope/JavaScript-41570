0b7820d6e582af04e87ebd69dafcae453c6df237
Joaquitope <joaquin.perez98@hotmail.com>
2022-08-17 18:50:56 -0300

    agregue css y otro html

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
- function calcularCuotas(){
-    let monto = parseInt(prompt("Ingrese un monto a calcular. (Ingrese 0 para salir)"));
-    let cuotas = parseInt( prompt("Ingrese cantidad de cuotas deseasas (MAX 12 con intereses)"));
-    while (monto != 0){
-    if (cuotas > 12){
-        alert("La cantidad de cuotas no puede ser mayor a 12")
-    }
-    else{
-        alert("Las cuotas son de $" + monto/cuotas * 1.25)
-    }
-    monto = parseInt(prompt("Ingrese un monto a calcular. (Ingrese 0 para salir)"));
-    cuotas = parseInt( prompt("Ingrese cantidad de cuotas deseasas (MAX 12)"));
-    }
-    alert("Ciclo finalizado");
+let productos = [
+    { Modelo: 787, Fabricante: "Boeing", Aerolinea: "Aeromexico", precio: 20000 },
+    { Modelo: 737, Fabricante: "Boeing", Aerolinea: "Aerolineas Argentinas", precio: 19000 },
+    { Modelo: 777, Fabricante: "Boeing", Aerolinea: "Fly Emirates", precio: 18000 },
+    { Modelo: 330, Fabricante: "Airbus", Aerolinea: "Aerolineas Argentinas", precio: 24000 },
+]
+
+
+function carritoCompras(){
+    let precioMinimo = parseInt(prompt("Ingrese un precio minimo. (Ingrese 0 para salir)"));
+    let precioMaximo = parseInt(prompt("Ingrese un monto a maximo. (Ingrese 0 para salir)"));
+    
+    const productosFiltrados = productos.filter((producto) => producto.precio > precioMinimo && producto.precio < precioMaximo);
+    alert("Los productos que aplican al filtro son " + productosFiltrados.length)
+    productosFiltrados.forEach(element => {
+        alert("Modelo: " + element.Modelo + " , " + "Fabricante: " + element.Fabricante + " , " + "Aerolinea: " + element.Aerolinea + " , " + "Precio: " + element.precio)
+    });
 }
 
-calcularCuotas()
+carritoCompras()
+
+
+
