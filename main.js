f4a004a620d98da042a1e9b8d069e581926af01f
Joaquitope <joaquin.perez98@hotmail.com>
2022-08-23 13:41:39 -0300

    agregue form y evento

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-function mostrarMsj() {
+function formulario() {
+    const nombre = document.getElementById("nombre").value;
+    const valor = document.getElementById("valor").value;
+    const cuotas = document.getElementById("cuotas").value;
+
+    document.getElementById("msj").innerHTML = "Hola " + nombre + " " + "el precio final en "  + cuotas + " " + "cuotas seria de: " + valor/cuotas; 
+}
+
+
+
+/* function mostrarMsj() {
     let person = prompt("Ingrese su nombre ");
     if (person != null) {
       document.getElementById("usuario").innerHTML =
       "Hola " + person + " " + "podes ver nuestros productos en la seccion PRODUCTOS!";
     }
   }
-
+ */
 /* let productos = [
     { Modelo: 787, Fabricante: "Boeing", Aerolinea: "Aeromexico", precio: 20000 },
     { Modelo: 737, Fabricante: "Boeing", Aerolinea: "Aerolineas Argentinas", precio: 19000 },
